831121d645ccc8baf282b1d252b22348c8db756a
picafern-commits <pica.fern@gmail.com>
2026-04-01 16:03:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// FIREBASE
 const firebaseConfig = {
   apiKey: "AIzaSyCSgw4rhBLW5mq4QClulubf6e0hf5lDJbo",
   authDomain: "toner-manager-756c4.firebaseapp.com",
@@ -10,36 +11,31 @@ const db = firebase.firestore();
 let stockGlobal = [];
 
 
-// NAV
-function mudarPagina(p){
-  ["impressoras","computadores","config"].forEach(id=>{
-    let el = document.getElementById(id);
-    if(el) el.style.display="none";
-  });
-  document.getElementById(p).style.display="block";
-}
-
-
-// ID
+// GERAR ID
 async function gerarID(){
   const ref = db.collection("config").doc("contador");
 
   return db.runTransaction(async (t)=>{
     const doc = await t.get(ref);
+
     let numero = doc.exists ? doc.data().valor + 1 : 1;
     t.set(ref,{valor:numero});
+
     return "TON-" + String(numero).padStart(4,"0");
   });
 }
 
 
-// ADD
-async function disponivel(){
+// ADICIONAR
+window.disponivel = async function(){
 
-  let eq = document.getElementById("equipamento").value;
-  let loc = document.getElementById("localizacao").value;
+  let eq = equipamento.value;
+  let loc = localizacao.value;
   let cor = document.getElementById("cor").value;
   let data = document.getElementById("data").value;
+
+  if(!loc) loc="Sem Localização";
+  if(!data) data="Não tem Data";
 
   if(!eq || !cor){
     alert("Preenche tudo");
@@ -51,12 +47,13 @@ async function disponivel(){
   await db.collection("stock").add({
     idInterno:id,
     equipamento:eq,
-    localizacao:loc || "Sem Localização",
+    localizacao:loc,
     cor:cor,
-    data:data || "Sem Data",
+    data:data,
     created:new Date()
   });
-}
+
+};
 
 
 // STOCK
@@ -66,29 +63,22 @@ db.collection("stock").orderBy("created","desc").onSnapshot(snap=>{
 
   document.getElementById("countStock").innerText = snap.size;
 
-  let lista = document.getElementById("listaStock");
-  lista.innerHTML="";
+  if(snap.size < 5){
+    document.getElementById("countStock").style.color="red";
+  }
 
   snap.forEach(doc=>{
     let t = doc.data();
     t.idDoc = doc.id;
     stockGlobal.push(t);
-
-    lista.innerHTML+=`
-      <div class="card">
-        <input type="checkbox" onchange="usar('${doc.id}')">
-        <b>${t.idInterno}</b><br>
-        ${t.equipamento} - ${t.cor}<br>
-        ${t.localizacao}
-      </div>
-    `;
   });
+
+  renderStock(stockGlobal);
 });
 
 
 // HISTÓRICO
 db.collection("historico").onSnapshot(snap=>{
-
   document.getElementById("countUsados").innerText = snap.size;
 
   let lista = document.getElementById("listaHistorico");
@@ -100,39 +90,57 @@ db.collection("historico").onSnapshot(snap=>{
     lista.innerHTML+=`
       <div class="card">
         <b>${t.idInterno}</b><br>
-        ${t.equipamento} - ${t.cor}
+        ${t.equipamento} - ${t.cor}<br>
+        ${t.localizacao}
+        <button class="delete" onclick="apagar('${doc.id}')">❌</button>
       </div>
     `;
   });
 });
 
 
+// RENDER STOCK
+function renderStock(lista){
+  let div=document.getElementById("listaStock");
+  div.innerHTML="";
+
+  lista.forEach(t=>{
+    div.innerHTML+=`
+      <div class="card">
+        <input type="checkbox" onchange="usar('${t.idDoc}')">
+        <b>${t.idInterno}</b><br>
+        ${t.equipamento} - ${t.cor}<br>
+        ${t.localizacao}
+      </div>
+    `;
+  });
+}
+
+
+// FILTRO
+window.filtrar = function(){
+  let txt = document.getElementById("search").value.toLowerCase();
+
+  let filtrado = stockGlobal.filter(t =>
+    t.idInterno.toLowerCase().includes(txt)
+  );
+
+  renderStock(filtrado);
+};
+
+
 // USAR
-async function usar(id){
+window.usar = async function(id){
+
   let ref = db.collection("stock").doc(id);
   let snap = await ref.get();
 
   await db.collection("historico").add(snap.data());
   await ref.delete();
-}
+};
 
 
-// FILTRO
-function filtrar(){
-  let txt = document.getElementById("search").value.toLowerCase();
-
-  let lista = document.getElementById("listaStock");
-  lista.innerHTML="";
-
-  stockGlobal
-    .filter(t => (t.localizacao || "").toLowerCase().includes(txt))
-    .forEach(t=>{
-      lista.innerHTML+=`
-        <div class="card">
-          <b>${t.idInterno}</b><br>
-          ${t.equipamento} - ${t.cor}<br>
-          ${t.localizacao}
-        </div>
-      `;
-    });
-}
+// APAGAR
+window.apagar = async function(id){
+  await db.collection("historico").doc(id).delete();
+};
